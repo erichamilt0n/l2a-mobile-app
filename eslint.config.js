ce93bc00f5ece2a7657d634fe42b53268f4c5bd2
Eric Hamilton <j.eric.hamilton@gmail.com>
2024-12-22 17:15:33 -0500

    fix: resolve ESLint errors

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,21 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from '@typescript-eslint/eslint-plugin'
+import * as tseslint from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 
 export default [
   js.configs.recommended,
   {
-    ignores: ['dist/**']
+    ignores: ['dist/**', 'vite.config.*']
   },
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: {
-        ...globals.browser
+        ...globals.browser,
+        React: 'readonly'
       },
       parser: tsParser,
       parserOptions: {
@@ -29,6 +30,7 @@ export default [
     },
     rules: {
       ...tseslint.configs.strict.rules,
+      '@typescript-eslint/no-non-null-assertion': 'off',
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
       'react-refresh/only-export-components': [
