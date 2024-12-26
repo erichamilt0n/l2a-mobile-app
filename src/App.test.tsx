19323ca0a615b352a1e116bef5c514985c8ff98a
Eric Hamilton <j.eric.hamilton@gmail.com>
2024-12-26 15:30:02 -0500

    test: fix App test
    - Remove BrowserRouter wrapper since App already includes one

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,9 @@
-import { render, screen } from '@testing-library/react'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
 describe('App', () => {
   it('renders without crashing', () => {
-    render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    )
-    expect(screen.getByText(/login/i)).toBeInTheDocument()
-  })
-})
+    render(<App />);
+    expect(screen.getByText(/login/i)).toBeInTheDocument();
+  });
+});
