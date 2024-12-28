a2dd8faff09a3c18539ee6834596e7c22b40822f
Eric Hamilton <j.eric.hamilton@gmail.com>
2024-12-28 16:36:04 -0500

    fix: resolve eslint errors in test files
    - Add proper globals for test functions in eslintrc
    - Add React imports where needed
    - Clean up unused imports
    - Fix test utilities configuration

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,9 +1,10 @@
-import { render } from '@testing-library/react'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 // eslint-disable-next-line react-refresh/only-export-components
-export * from '@testing-library/react'
+export * from '@testing-library/react';
 
 export function renderWithRouter(ui: React.ReactElement) {
-  return render(ui, { wrapper: BrowserRouter })
+  return render(ui, { wrapper: BrowserRouter });
 }
